9f76cb70486e5e98b3896b3b810b2398625d0789
Nessiezhao <Nessie_zhao@163.com>
2018-07-24 14:43:58 +0800

    add test.c

---
+++ pro10/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+struct S3
+{
+    double d;
+    char c;
+    int i;
+};
+
+struct S4
+{
+    char c1;
+    struct S3 s3;
+    double d;
+};
+
+int main()
+{
+    printf("%d\n",sizeof(struct S4));
+    return 0;
+}
